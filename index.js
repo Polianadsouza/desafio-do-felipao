06d423f886414d81ae659aac80932ccfa4355001
Poliana Souza <polianareisdesouza@gmail.com>
2024-11-09 16:41:18 +0000

    desafio concluido

---
+++ index.js
@@ -0,0 +1,23 @@
+let nomeHeroi = "Oliver";
+let pontosDeVida = 7002;
+let nivel;
+
+if (pontosDeVida <= 1001) {
+    nivel = "ferro"
+} else if (pontosDeVida <= 2000) {
+    nivel = "bronze"
+} else if (pontosDeVida >= 2001 && pontosDeVida <= 5000) {
+    nivel = "prata"
+} else if (pontosDeVida >= 5001 && pontosDeVida < 7000 ) {
+    nivel = "ouro"
+} else if (pontosDeVida >= 7001 && pontosDeVida < 8000) {
+    nivel = "platina"
+} else if (pontosDeVida >= 8001 && pontosDeVida < 9000) {
+    nivel = "ascendente"
+} else if (pontosDeVida >= 9001 && pontosDeVida < 10000) {
+    nivel = "imortal"
+} else if (pontosDeVida >= 10000) {
+    nivel = "radiante"
+}
+
+console.log("O heroi " + nomeHeroi + " está no nivel " + nivel);
